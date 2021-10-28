149735beee2e6b8e958d32952ec7a896aa0716e7
superevilchen <55413963+superevilchen@users.noreply.github.com>
2021-10-28 11:28:00 +0300

    Update DirectorySearch.java

--- DirectorySearch.java
+++ DirectorySearch.java
@@ -1,39 +1,21 @@
-package lab4;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.concurrent.RecursiveAction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 
-public class DirectorySearch extends RecursiveAction{
+public class DirectorySearch {
 	
-	private String directory;
 	private String fileExt;
 	private List<String> fileList;
 	
-	public DirectorySearch(String directory, String fileExt, List<String> fileList) {
-		this.directory = directory;
+	public DirectorySearch(String fileExt, List<String> fileList) {
 		this.fileExt = fileExt;
 		this.fileList = fileList;
-	}
-	
-	
-	@Override
-	protected void compute() {
-		
-
-		}
-		
-//	    private List<CustomRecursiveAction> createSubtasks() {
-//	        List<CustomRecursiveAction> subtasks = new ArrayList<>();
-//	    }
-//	    
+	} 
 	    
 	    private void getDirectory(String directory) {
 			File f = new File(directory);
@@ -59,8 +41,5 @@ public class DirectorySearch extends RecursiveAction{
 					}
 			}
 	    }
-
 }
-	    
-	    
 }
